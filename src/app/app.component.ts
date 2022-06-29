7516de517def2c09c7f8b8e0d91ed7e31aabbd1d
Chanikya3245 <chanikya.avala@terralogic.com>
2022-06-29 17:47:49 +0530

    updates to deploy to heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,65 @@
+// import { Component } from '@angular/core';
+// // import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+
+
+// // export interface Vegetable {
+// //   name: string;
+// // }
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+  //  selector: 'form-field-appearance-example',
+  // templateUrl: 'form-field-appearance-example.html',
+// })
+// export class AppComponent { }
+
+  
+//   vegetables: Vegetable[] = [
+//     { name: 'apple' },
+//     { name: 'banana' },
+//     { name: 'strawberry' },
+//     { name: 'orange' },
+//     { name: 'kiwi' },
+//     { name: 'cherry' },
+//     { name: 'apple' },
+//     { name: 'banana' },
+//     { name: 'strawberry' },
+//     { name: 'orange' },
+//     { name: 'kiwi' },
+//     { name: 'cherry' },
+    
+
+//   ];
+
+// drop(event: CdkDragDrop<Vegetable[]>) {
+//     moveItemInArray(this.vegetables, event.previousIndex, event.currentIndex);
+//   }
+// }
+
+
 import { Component } from '@angular/core';
+
+export interface EmployeeDetail {
+  id: number;
+  name: string;
+  role: string;
+  age: number;
+}
+
+const ELEMENT_DATA: EmployeeDetail[] = [
+  {id: 1001, name: 'Andrew',   role: 'Software', age: 32},
+  {id: 1002, name: 'Jack',     role: 'Software', age: 28},
+  {id: 1003, name: 'Max',      role: 'Software', age: 29},
+  {id: 1004, name: 'Bijo',     role: 'Software', age: 31},
+  {id: 1005, name: 'Cathy',    role: 'Software', age: 23},
+  {id: 1006, name: 'Robert',   role: 'Software', age: 28},
+  {id: 1007, name: 'Candice',  role: 'Software', age: 24},
+  {id: 1008, name: 'White',    role: 'Software', age: 33},
+  {id: 1009, name: 'James',    role: 'Software', age: 27},
+  {id: 1010, name: 'Vince',    role: 'Software', age: 30},
+];
 
 @Component({
   selector: 'app-root',
@@ -7,4 +68,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'card';
+
+  displayedColumns: string[] = ['id', 'name', 'role', 'age'];
+  dataSource = ELEMENT_DATA;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+
+import { DragDropModule} from '@angular/cdk/drag-drop';
+import { TableComponent } from './table/table.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    
+    MatTableModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatChipsModule,
+    DragDropModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
